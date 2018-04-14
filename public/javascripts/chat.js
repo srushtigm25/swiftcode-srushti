b0a854c023c51fd4d7fcaee66f1fae0dd6aad490
srushtigm25 <srushtigm25@gmail.com>
2018-04-14 12:13:22 +0000

    style updated

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,26 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'hello'
+         },
+        {
+
+            'sender': 'BOT',
+            'text': 'hiiiiii'
+         },
+        {
+
+            'sender': 'USER',
+            'text': 'ssssuuuppp'
+         },
+        {
+
+            'sender': 'BOT',
+            'text': '!!!!!!!!!!'
+         }
+	];
 
 });

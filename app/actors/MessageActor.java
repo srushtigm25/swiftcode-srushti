91f7d6d7ca61f9909155b369c563d0a5a241b4fe
srushtigm25 <srushtigm25@gmail.com>
2018-04-15 06:05:12 +0000

    added messageActor

--- app/actors/MessageActor.java
+++ app/actors/MessageActor.java
@@ -1,16 +1,48 @@
 package actors;
 
 
+import akka.actor.ActorRef;
+import akka.actor.Props;
 import akka.actor.UntypedActor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import data.FeedResponse;
+import data.Message;
+import services.FeedService;
+import services.NewsAgentService;
 
-
+import java.util.UUID;
 
 
 public class MessageActor extends UntypedActor {
+    private final ActorRef out;
 
+    private FeedService feedService = new FeedService();
+    private NewsAgentService newsAgentService = new NewsAgentService();
+    private FeedResponse feedResponse = new FeedResponse();
+
+
+    public static Props props(ActorRef out) {
+        return Props.create(MessageActor.class, out);
+    }
+
+    public MessageActor(ActorRef out) {
+        this.out = out;
+    }
 
     @Override
     public void onReceive(Object message) throws Throwable {
-
+        ObjectMapper objectMapper = new ObjectMapper();
+        Message messageObject = new Message();
+        if (message instanceof String) {
+            messageObject.text = (String) message;
+            messageObject.sender = Message.Sender.USER;
+            out.tell(objectMapper.writeValueAsString(messageObject), self());
+            String query = newsAgentService.getNewsAgentResponse("Find " + message, UUID.randomUUID()).query;
+            FeedResponse feedResponse = feedService.getFeedByQuery(query);
+            messageObject.text = (feedResponse.title == null) ? "No results found" : "Showing results for: " + query;
+            messageObject.feedResponse = feedResponse;
+            messageObject.sender = Message.Sender.BOT;
+            out.tell(objectMapper.writeValueAsString(messageObject), self());
+        }
     }
 }
--- app/data/Message.java
+++ app/data/Message.java
@@ -2,4 +2,12 @@ package data;
 
 public class Message {
 
+    public String text;
+    public FeedResponse feedResponse;
+    public Sender sender;
+    public enum Sender {
+        BOT, USER
+    }
+
+
 }
